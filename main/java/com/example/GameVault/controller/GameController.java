1c0e0b24a807038f355356540c2b5188cc4fcc3a
rodvillarg <jesus.villegas261186@potros.itson.edu.mx>
2026-06-17 13:14:45 -0700

    Add files via upload

---
+++ main/java/com/example/GameVault/controller/GameController.java
@@ -0,0 +1,74 @@
+package com.example.GameVault.controller;
+
+import com.example.GameVault.model.Juego;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.ui.Model;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class GameController {
+
+    private static List<Juego> juegosDb = new ArrayList<>();
+    private static  long idCounter = 1;
+    private static final String UPLOAD_DIR = "src/main/resources/static/uploads/";
+
+    @GetMapping("/fragments-demo")
+    public String fragmentsDemo() {
+        return "fragments-demo";
+    }
+
+    @GetMapping({"/","/juegos"})
+    public String listarJuegos(Model model) {
+        model.addAttribute("juegos", juegosDb);
+        return "juegos";
+    }
+
+    @GetMapping("/juegos/nuevo")
+    public String mostrarFormulario() {
+        return "formulario";
+    }
+
+    @PostMapping("/juegos")
+    public String guardarJuego(@RequestParam("titulo") String titulo,
+                               @RequestParam("descripcion") String descripcion,
+                               @RequestParam("portada")MultipartFile portada
+    ) {
+        String nombreArchivo = "default.png";
+
+        if(!portada.isEmpty()){
+            try{
+                Path uploadPath = Paths.get(UPLOAD_DIR);
+                if(!Files.exists(uploadPath)){
+                    Files.createDirectories(uploadPath);
+                }
+
+                nombreArchivo = UUID.randomUUID().toString()+"_"+portada.getOriginalFilename();
+
+
+                Path filePath = uploadPath.resolve(nombreArchivo);
+
+                Files.copy(portada.getInputStream(), filePath);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Juego nuevoJuego = new Juego(idCounter++, titulo, descripcion, nombreArchivo);
+            juegosDb.add(nuevoJuego);
+
+            return "redirect:/juegos";
+        }
+
+        return "formulario";
+    }
+
+}
